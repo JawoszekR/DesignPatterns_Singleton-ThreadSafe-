090a677273a29f05d37c7de76ca398a188d1497f
JawoszekR <jawoszekr@gmail.com>
2019-04-16 18:42:05 +0200

    First version, unsafe

---
+++ DesignPatterns_Singleton-ThreadSafe-/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DesignPatterns_Singleton_ThreadSafe_
+{
+    class Singleton
+    {
+        private static Singleton _instance;
+        private int _counter;
+        private Singleton()
+        {
+            _counter = 0;
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public void AddOneToCounter()
+        {
+            var state = _counter;
+            Thread.Sleep(6000);
+            _counter = state + 1;
+        }
+
+        public int GetCounter()
+        {
+            return _counter;
+        }
+    }
+}
